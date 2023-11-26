34992012514549c39883df198352a435be60180d
Dinushan-S <selvandinu18@gmail.com>
2023-11-26 22:14:07 +0530

    day07 project

---
+++ day07_random_quote_generator_redux/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Quote from './component/Quote';
+function App() {
+  return (
+    <div className='app row'>
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
