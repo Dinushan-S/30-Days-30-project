e70ae36bc5b45575c71a9ac13e8365491a1f4df1
Dinushan-S <selvandinu18@gmail.com>
2023-12-01 23:07:46 +0530

    day12 crypto listing app complete

---
+++ day11_cryptocurrency_listing_app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <div className='navbar navbar-dark bg-dark'>
+                <div className='w-100 text-light'>
+                    <h3>CryptoCurrency Information App</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
