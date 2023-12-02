fe1fe876f789e57ec1845290b8b35e681668aad5
Dinushan-S <selvandinu18@gmail.com>
2023-12-02 21:35:57 +0530

    day13 project spaceX api call

---
+++ day12_spacex_detail/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <nav className='navbar navbar-dark bg-dark'>
+            <div className='text-light'>
+                <h2>SpaceX Launch</h2>
+            </div>
+        </nav>
+    )
+}
