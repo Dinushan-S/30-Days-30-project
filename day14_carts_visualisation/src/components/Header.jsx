3832584099fff424228884aa98f2bf37e69bc678
Dinushn-S <selvandinu18@gmail.com>
2023-12-03 21:36:52 +0530

    day14 complete

---
+++ day14_carts_visualisation/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <nav className='navbar navbar-dark bg-dark'>
+            <div className='w-100 text-light'>GDP Charts</div>
+        </nav>
+    )
+}
